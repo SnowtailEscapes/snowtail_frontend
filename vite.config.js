b62762cc1a143700b8a93fe2cd889c3b6981989e
11111Divyam11111 <126981502+11111Divyam11111@users.noreply.github.com>
2024-06-25 00:57:52 +0530

    undo many changes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
 
-dotenv.config() // Load .env file
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: parseInt(import.meta.env.VITE_PORT) || 3000, // Use the VITE_PORT environment variable
-  },
-  build: {
-    outDir: 'dist',
-  },
-});
+})
