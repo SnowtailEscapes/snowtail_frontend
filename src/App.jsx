6de392bb0331fd624a6cc9dd91f459066428e642
11111Divyam11111 <126981502+11111Divyam11111@users.noreply.github.com>
2024-07-27 21:43:24 +0530

    currency converter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./components/About";
 import PrivacyPolicy from "./components/PrivacyPolicy";
