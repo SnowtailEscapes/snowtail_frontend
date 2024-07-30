c6a1dc0597e890970426604bdd7a59bc3488beb5
Divyam <126981502+11111Divyam11111@users.noreply.github.com>
2024-07-30 21:05:28 +0530

    Update International.jsx

--- src/components/InternationalCards/International.jsx
+++ src/components/InternationalCards/International.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import a from "../../styles/International.module.css";
+import CurrencyConverter from "../../getApi/CurrencyConverter";
+import { useDispatch } from "react-redux";
+import { fetchCurrencyRates } from "../../redux/slice/currencySlice";
 
 const International = ({ to, title, location, price, duration, image }) => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCurrencyRates());
+  }, [dispatch]);
   return (
     <div className={`${a.cardContainer} shadow-lg rounded-lg overflow-hidden`}>
       <div className="md:flex">
@@ -21,8 +30,8 @@ const International = ({ to, title, location, price, duration, image }) => {
             <li className={a.disc}>International Tour</li>
           </ul>
           <div>
-            <h2 className="text-2xl font-bold mb-2 font-arimo">{title}</h2>
-            <p className={`text-gray-600 mb-4 ${a.boston}`}>{location}</p>
+            <h2 className="text-2xl font-bold mb-2">{title}</h2>
+            <p className="text-gray-600 mb-4">{location}</p>
             <div className="flex space-x-2 mb-4">
               <span className="bg-green-100 text-green-600 px-3 py-1 rounded-full text-xs">All meals</span>
               <span className="bg-green-100 text-green-600 px-3 py-1 rounded-full text-xs">3 star resort</span>
@@ -30,12 +39,15 @@ const International = ({ to, title, location, price, duration, image }) => {
             </div>
             <div className="flex justify-between items-center">
               <h3 className="text-xl font-semibold">Price</h3>
-              <h3 className="text-2xl text-main-brand">{price}</h3>
+              <h3 className="text-2xl text-color1 flex-nowrap flex">
+                <CurrencyConverter price={price} />
+                <p>/person</p>
+              </h3>
             </div>
           </div>
           <div className="mt-4">
             <Link to={to}>
-              <button className="btn bg-main-brand text-white text-nowrap w-full py-2 rounded-lg">
+              <button className="btn bg-color1 text-white text-nowrap w-full py-2 rounded-lg">
                 View Details
               </button>
             </Link>
@@ -50,7 +62,7 @@ International.propTypes = {
   title: PropTypes.string.isRequired,
   to: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  price: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
   duration: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
 
